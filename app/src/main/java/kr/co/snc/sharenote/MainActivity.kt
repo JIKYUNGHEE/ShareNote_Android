32bab0d708b02b177b3c3a4672cab411e8464e5b
khji <khji@mobileleader.com>
2020-12-09 11:56:41 +0900

    Google login 작업

--- app/src/main/java/kr/co/snc/sharenote/MainActivity.kt
+++ app/src/main/java/kr/co/snc/sharenote/MainActivity.kt
@@ -1,35 +1,52 @@
 package kr.co.snc.sharenote
 
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
 import kr.co.snc.sharenote.databinding.ActivityMainBinding
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
-   private val TAG:String? = "MainActivity"
+    private val TAG: String? = "MainActivity"
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        var RC_SIGN_IN: Int = 1;
+    }
 
     // 로그인 공통 callback 구성
     val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
             Log.e(TAG, "로그인 실패", error)
-        }
-        else if (token != null) {
+        } else if (token != null) {
             Log.i(TAG, "로그인 성공 ${token.accessToken}")
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initGoogleLogin()
+        binding.loginGoogle.setSize(SignInButton.SIZE_STANDARD)
+        binding.loginGoogle.setOnClickListener { view ->
+            val signInIntent: Intent = mGoogleSignInClient?.signInIntent
+            startActivityForResult(signInIntent, RC_SIGN_IN)
+
+        }
 
         binding.loginKakao.setOnClickListener { view ->
             // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
@@ -38,6 +55,51 @@ class MainActivity : AppCompatActivity() {
             } else {
                 LoginClient.instance.loginWithKakaoAccount(applicationContext, callback = callback)
             }
+        }
+    }
+
+    private fun initGoogleLogin() {
+        val gso =
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        if (account != null) {
+            binding.loginSuccess.visibility = View.VISIBLE
+            Log.d("MainActivity", "account: $account, email: ${account.email}")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
         }
     }
 }
