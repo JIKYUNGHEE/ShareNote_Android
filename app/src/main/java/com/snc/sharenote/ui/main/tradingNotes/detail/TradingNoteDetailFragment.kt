4791b8b85678951f15baf95a602a7de81f94f4f3
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-03-16 17:06:46 +0900

    - [작성완료] 데이터 넘기는 중

--- app/src/main/java/com/snc/sharenote/ui/main/tradingNotes/detail/TradingNoteDetailFragment.kt
+++ app/src/main/java/com/snc/sharenote/ui/main/tradingNotes/detail/TradingNoteDetailFragment.kt
@@ -1,15 +1,11 @@
 package com.snc.sharenote.ui.main.tradingNotes.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.snc.sharenote.R
 import com.snc.sharenote.databinding.FragmentTradingNoteDetailBinding
 import com.snc.sharenote.ui.main.tradingNotes.write.TradingNoteWriteViewModel
 
